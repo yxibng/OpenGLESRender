6cf09da8eb4a10b37e4eeddea35fff7f30902d94
yxibng <yaoxiaobing@axzt.net>
2023-11-17 16:14:46 +0800

    first commit

---
+++ GLRender/GLRender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GLRender
+//
+//  Created by yxibng on 2023/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
